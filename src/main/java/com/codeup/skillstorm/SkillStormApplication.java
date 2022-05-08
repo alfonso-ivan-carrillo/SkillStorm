01c79a86e65bb048ec57b6f9b56e2edaa13a28ee
Alfonso Carrillo <alfonso.ivan.carrillo@gmail.com>
2022-05-08 16:35:40 -0500

    first commit

---
+++ src/main/java/com/codeup/skillstorm/SkillStormApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.skillstorm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillStormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillStormApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeup/skillstorm/SkillStormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.skillstorm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillStormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
