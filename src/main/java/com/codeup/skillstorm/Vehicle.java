4eb2cdb0ba7b08e3c3c979f7f94116b733b2a9fd
Alfonso Carrillo <alfonso.ivan.carrillo@gmail.com>
2022-05-09 16:07:41 -0500

    data types

---
+++ src/main/java/com/codeup/skillstorm/Vehicle.java
@@ -0,0 +1,19 @@
+package com.codeup.skillstorm;
+
+public class Vehicle {
+    // variables
+    int speed = 200000000;
+    byte wheels = 4;
+    short weight = 2000;
+    boolean running = false;
+    long serial = 123456789123456789L;
+    double fuelRemaining = 100.1234567890123f;
+
+    String make = "Toyota";
+
+    Vehicle toy;
+    // method
+    void accelerate(){
+        int count = 0;
+    }
+}
