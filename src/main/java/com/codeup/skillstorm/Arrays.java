96cf3a39008e74b312f239645accf8d30f8603be
Alfonso Carrillo <alfonso.ivan.carrillo@gmail.com>
2022-05-09 16:12:57 -0500

    arrays skillstorm lesson

---
+++ src/main/java/com/codeup/skillstorm/Arrays.java
@@ -0,0 +1,20 @@
+package com.codeup.skillstorm;
+
+public class Arrays {
+
+    public static void main(String[] args) {
+        // array - indicate the type of array, like in this case int and [], after the = sign, in[6] the number of items in array
+        int[] array = new int[6];
+
+        array[0] = 155;
+        array[5] = 77;
+        System.out.println(array[5]);
+
+        // array initializer
+        String[] words = {"abc", "def", "jkl"};
+
+        words[0] = "dee";
+
+
+    }
+}
