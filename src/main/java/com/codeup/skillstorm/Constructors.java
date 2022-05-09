7209643a204e8a38304c6d4daf982ee7032b6a16
Alfonso Carrillo <alfonso.ivan.carrillo@gmail.com>
2022-05-09 16:27:51 -0500

    constructors lesson

---
+++ src/main/java/com/codeup/skillstorm/Constructors.java
@@ -0,0 +1,45 @@
+package com.codeup.skillstorm;
+
+class Pizza {
+    int slices;
+    String topping;
+    public void eat(){
+        slices = slices - 1;
+    }
+
+    public Pizza() {
+    }
+
+    public Pizza(int slices) {
+        this.slices = slices;
+    }
+
+    public Pizza(String topping) {
+        this.topping = topping;
+    }
+
+    public Pizza(int slices, String topping) {
+        this.slices = slices;
+        this.topping = topping;
+    }
+}
+
+public class Constructors {
+
+
+
+    public static void main(String[] args) {
+
+    Pizza dominiosPizza = new Pizza();
+    dominiosPizza.slices = 8;
+        System.out.println(dominiosPizza.slices);
+
+    Pizza pie = new Pizza(6);
+    dominiosPizza.eat();
+
+        System.out.println(dominiosPizza.slices);
+        System.out.println(pie.slices);
+
+
+    }
+}
