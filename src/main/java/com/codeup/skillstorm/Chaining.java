804af010c7f025fc22bc741d4242154c92f1d643
Alfonso Carrillo <alfonso.ivan.carrillo@gmail.com>
2022-05-09 20:44:38 -0500

    constructor chainging

---
+++ src/main/java/com/codeup/skillstorm/Chaining.java
@@ -0,0 +1,32 @@
+package com.codeup.skillstorm;
+
+public class Chaining {
+
+    public static void main(String[] args) {
+        JavaDeveloper dev = new JavaDeveloper( "Java");
+
+    }
+}
+
+class Employee{
+    String name = "Dan Pickles";
+    public Employee(){
+        System.out.println("Employee");
+    }
+}
+
+class JavaDeveloper extends Employee {
+    String language;
+
+    public JavaDeveloper(){
+        super();
+        System.out.println(name + " JavaDeveloper");
+    }
+
+    public JavaDeveloper(String language){
+        this();
+        this.language = language;
+        System.out.println("the language is " + language);
+
+    }
+}
