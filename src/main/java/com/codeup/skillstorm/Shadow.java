d457bbdbbd19f8d9ee098dde2f3871fdbd226f98
Alfonso Carrillo <alfonso.ivan.carrillo@gmail.com>
2022-05-09 20:29:58 -0500

    variable shadowing

---
+++ src/main/java/com/codeup/skillstorm/Shadow.java
@@ -0,0 +1,32 @@
+package com.codeup.skillstorm;
+
+public class Shadow {
+
+    public static void main(String[] args) {
+        Child child = new Child();
+        int returned = child.getX();
+        System.out.println(returned);
+    }
+}
+
+class Parent {
+    int x = 10;
+}
+
+class Child extends Parent {
+    int x = 5;
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getX(){
+        // returns the value of the child class
+        return x;
+        // returns the value of the current method by using this.x
+        // return this.x;
+
+        // super.x returns the value of the parent class Parent
+        // return super.x;
+    }
+}
