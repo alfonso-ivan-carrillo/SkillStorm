1179878ac55844f12ca2973ff93ddbfd8512914a
Alfonso Carrillo <alfonso.ivan.carrillo@gmail.com>
2022-05-15 15:04:11 -0500

    working on solution

--- src/main/java/com/codeup/skillstorm/drive/Vehicle.java
+++ src/main/java/com/codeup/skillstorm/drive/Vehicle.java
@@ -15,14 +15,55 @@ package com.codeup.skillstorm.drive;
  *    You should not have any public variables in this class. All fields must be private.
  *    Note: getter methods are named differently for boolean values.
  */
-public class Vehicle {
+public class Vehicle implements Driveable{
 
-    public String year;
-    public String make;
+    private String year;
+    private String make;
     String model;
     String plateNumber;
-    public boolean inTransit;
+    private boolean inTransit;
 
+    public void drive() {
+        inTransit = true;
+    }
 
+    public String getYear() {
+        return year;
+    }
 
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getPlateNumber() {
+        return plateNumber;
+    }
+
+    public void setPlateNumber(String plateNumber) {
+        this.plateNumber = plateNumber;
+    }
+
+    public boolean isInTransit() {
+        return inTransit;
+    }
+
+    public void setInTransit(boolean inTransit) {
+        this.inTransit = inTransit;
+    }
 }
