81ecbd40bb6698818ad1a8190b1bba857c24bdaf
Alfonso Carrillo <alfonso.ivan.carrillo@gmail.com>
2022-05-18 16:04:42 -0500

    skillstorm for-loop lesson

---
+++ src/main/java/com/codeup/skillstorm/ForLoop.java
@@ -0,0 +1,15 @@
+package com.codeup.skillstorm;
+
+public class ForLoop {
+
+    static void loop(int max){
+        for (int i = 0; i <= max; i++){
+            System.out.println(i);
+        }
+    }
+
+
+    public static void main(String[] args) {
+    loop(10);
+    }
+}
