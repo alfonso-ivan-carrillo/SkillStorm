77318a9c82fa85de7b61cdcf96c0b877795a5b10
Alfonso Carrillo <alfonso.ivan.carrillo@gmail.com>
2022-05-23 13:31:18 -0500

    Math Ops SkillStorm lesson

---
+++ src/main/java/com/codeup/skillstorm/MathOps.java
@@ -0,0 +1,22 @@
+package com.codeup.skillstorm;
+
+public class MathOps {
+    public static void main(String[] args) {
+        int add = 2 + 2;
+        int subtract = 2 - 2;
+        int multiply = 2 * 2;
+        int divide = 2 / 2;
+
+        System.out.println(add);
+        System.out.println(subtract);
+        System.out.println(multiply);
+        System.out.println(divide);
+
+        // modulus
+        int remainder = 5 % 2;
+        System.out.println(remainder);
+
+        // String concat
+        System.out.println("Hello " + "World!" );
+    }
+}
