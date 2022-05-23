07d7b9c2d3e0962ee571f4dd18aa8855933d3af9
Alfonso Carrillo <alfonso.ivan.carrillo@gmail.com>
2022-05-23 13:21:30 -0500

    assignment operator skillstorm lesson

---
+++ src/main/java/com/codeup/skillstorm/Assign.java
@@ -0,0 +1,19 @@
+package com.codeup.skillstorm;
+
+public class Assign {
+
+    String instance = "var";
+
+    public static void main(String[] args) {
+        int local = 10;
+        boolean bool = true;
+
+        while (bool == true){
+            System.out.println("true");
+            bool = false;
+        }
+
+
+
+    }
+}
