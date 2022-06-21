c4b3aebdd4d7f277c44a1ed57162e4d09aff58c7
Alfonso Carrillo <alfonso.ivan.carrillo@gmail.com>
2022-06-21 10:47:15 -0500

    SkillStorm exception handling lesson

---
+++ src/main/java/com/codeup/skillstorm/Risky.java
@@ -0,0 +1,23 @@
+package com.codeup.skillstorm;
+
+import java.io.FileInputStream;
+
+public class Risky {
+
+
+
+    public static void main(String[] args) {
+//        math exception
+//        int x = 5 / 0;
+
+//        null pointer exception
+//        Object obj = null;
+//        obj.toString();
+//        array index out of bounds exception
+//        int[] arr = new int[5];
+//        arr[100] = 10;
+//        file not found exception - file does not exisit in this example
+//        FileInputStream file = new FileInputStream("file.text");
+
+    }
+}
