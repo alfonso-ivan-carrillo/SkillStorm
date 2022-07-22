af5d7f0b940d97ffe7f57ddb22988af99360eed5
Alfonso Carrillo <alfonso.ivan.carrillo@gmail.com>
2022-07-22 15:10:31 -0500

    SkillStorm hashcode lesson

--- src/main/java/com/codeup/skillstorm/Equals.java
+++ src/main/java/com/codeup/skillstorm/Equals.java
@@ -5,9 +5,16 @@ public class Equals {
     public static void main(String[] args) {
 
         Pizzas one = new Pizzas();
+        one.size = 14;
+        one.topping = "Pepperoni";
         Pizzas two = new Pizzas();
-        System.out.println(one == two);
-        System.out.println(one.equals(two));
+        two.size = 14;
+        two.topping = "Cheese";
+        System.out.println((one == two) + " 1");  // bad practice to use == in java when comparing
+        System.out.println(one.equals(two) + " 2");
+
+        System.out.println(one.hashCode());
+        System.out.println(two.hashCode());
 
     }
 }
@@ -15,7 +22,13 @@ public class Equals {
 class Pizzas {
     int size = 0;
     String topping = null;
-
+//    public int hashCode() {
+//        if (topping != null) {
+//            return size * topping.hashCode();
+//        } else {
+//            return size;
+//        }
+//    }
     public boolean equals(Object o){
         Pizzas other = (Pizzas) o;
         if(this.size == other.size){
@@ -25,4 +38,13 @@ class Pizzas {
         }
         return false;
     }
+
+    public int hashCode(){
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + size;
+        result = prime * result + ((topping == null) ? 0 : topping.hashCode());
+        return result;
+    }
+
 }
